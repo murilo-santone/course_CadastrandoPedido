4026015f9a42831137f27f711590f37cd8651f13
Murilo <murilo.santone@hotmail.com>
2021-07-08 16:27:01 -0300

    Exercício proposto pelo curso de C# - Primeiro commit com as entidades e com enumeração

---
+++ Pedido/Entities/Order.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using Pedido.Entities.Enum;
+
+namespace Pedido.Entities
+{
+    class Order
+    {
+        public DateTime Moment { get; set; }
+        public OrderStatus Status { get; set; }
+        public List<OrderItem> OrderItems { get; set; } = new List<OrderItem>();
+
+
+
+    }
+}
---
+++ Pedido/Entities/OrderItem.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pedido.Entities
+{
+    class OrderItem
+    {
+        public int Quantidade { get; set; }
+        public double Price { get; set; }
+
+
+
+        public OrderItem()
+        {
+
+        }
+
+        public OrderItem(int quantidade, double price)
+        {
+            Quantidade = quantidade;
+            Price = price;
+        }
+
+
+        public double SubTotal()
+        {
+            return Quantidade * Price;
+        }
+    }
+}
